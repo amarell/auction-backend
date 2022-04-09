2f56d1db25aa4df6737e2ed6c70869b9850a759e
amarell <mujagicamar@gmail.com>
2022-04-09 13:52:51 +0200

    Searching for auctions

--- API/controllers/auctionController.js
+++ API/controllers/auctionController.js
@@ -1,19 +1,62 @@
 const Auction = require("../models/auctionModel");
 const { authorize } = require("../routes/protectedRoute");
 
-module.exports.getAuctions = (req, res) => {
-  Auction.find((err, auctions) => {
-    if (err) {
-      res.status(400).json({
-        error: "Something went wrong!",
-      });
+module.exports.getAuctions = async (req, res) => {
+  try {
+    let searchQuery = req.query.search;
+
+    // TODO: add sorting by the number of bids and other params
+
+    if (searchQuery) {
+      const auctions = await Auction.aggregate([
+        [
+          {
+            $lookup: {
+              from: "bids",
+              localField: "bids",
+              foreignField: "_id",
+              as: "bids",
+            },
+          },
+          {
+            $match: {
+              $or: [
+                {
+                  item_name: {
+                    $regex: searchQuery,
+                    $options: "i",
+                  },
+                },
+                {
+                  item_description: {
+                    $regex: searchQuery,
+                    $options: "i",
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      ]);
+
+      res.status(200).json(auctions);
     } else {
-      res.status(200).json({
-        status: "Sucess",
-        data: auctions,
-      });
+      const auctions = await Auction.aggregate([
+        {
+          $lookup: {
+            from: "bids",
+            localField: "bids",
+            foreignField: "_id",
+            as: "bids",
+          },
+        },
+      ]).sort({ created_at: -1 });
+
+      res.status(200).json(auctions);
     }
-  });
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
 
 module.exports.postAuction = (req, res) => {
