e7a0314d2f19653be688efd4a7b82c9401aba686
amarell <mujagicamar@gmail.com>
2022-04-19 00:19:06 +0200

    Small validation update

--- validation.js
+++ validation.js
@@ -16,7 +16,7 @@ const registerValidation = (data) => {
 const loginValidation = (data) => {
   const schema = Joi.object({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required(),
   });
 
   return schema.validate(data);
